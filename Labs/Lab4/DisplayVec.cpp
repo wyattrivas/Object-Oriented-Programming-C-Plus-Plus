01ec0eeac3fa46a11fde2a0b477aa781e916cc30
wyattrivas <008181609@coyote.csusb.edu>
2023-10-30 21:12:50 -0700

    compiled files

---
+++ Labs/Lab4/DisplayVec.cpp
@@ -0,0 +1,1 @@
+//DisplayVec.cpp
---
+++ Labs/Lab4/Lab_4_vec.cpp
@@ -0,0 +1,34 @@
+/***
+ * CSE 2010 Fall 2023
+ * Lab #4
+ * Wyatt Rivas #008181609
+ * 10/30/2023
+ *
+ * For this lab we will be learning how to organize data using the data structures, arrays and vectors. 
+ * We will also learn the similarities and differences between the two of them in terms of how they contain, access and updates elements and how to pass them into functions. 
+ * Lastly, we will learn and understand how the data structures allocate memory and their relation to pointers.
+ * {Describe 1) what was most challenging and 2) how you solved/overcame the problem OR
+ * the directions of how you PLAN to solve it}
+ *
+***/
+
+#include <iostream>
+#include "ArrayVec.h"
+#include "DisplayVec.h"
+using namespace std;
+
+
+
+int main()
+{
+    // initialize vec
+
+    //initialize ArrayVec and DisplayVec
+
+
+    // populate with values
+
+    // display contents
+
+}
+
