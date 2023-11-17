4fde5dd54c554168ac68e0a53115dddfba727e59
wyattrivas <008181609@coyote.csusb.edu>
2023-11-17 11:41:32 -0800

    added code

--- Labs/Lab5/Lab_5.cpp
+++ Labs/Lab5/Lab_5.cpp
@@ -1,12 +1,13 @@
 /***
  * CSE 2010 Fall 2023
- * Lab #{n}
- * {Student Name} #{Coyote ID}
- * {Date}
+ * Lab #5
+ * Wyatt Rivas #008181609
+ * 11/10/2023
  *
  * {Description of lab AKA paraphrase "abstract" section from lab}
  * For this lab we will learn how to iterate through arrays and vectors to search for elements. 
- * They will monitor computation time and graph any findings. We will analyze the Big-O time to benchmark their search algorithms.
+ * They will monitor computation time and graph any findings. 
+ * We will analyze the Big-O time to benchmark their search algorithms.
  * {Describe 1) what was most challenging and 2) how you solved/overcame the problem OR
  * the directions of how you PLAN to solve it}
  *
@@ -23,7 +24,10 @@ int main()
     const int ITER_NUM_SIZES = 10;
     int arr_n[ITER_NUM_SIZES]=   {10, 100, 500, 1000, 5000, 10000, \
                                 50000, 100000, 200000, 500000};
-
+        
+        for (int i = 0; i < ITER_NUM_SIZES; i++) {
+            arr_n[i] *= 4;
+        } 
     // use this to set the range of values to generate to be from 0 to 999 (1000-1)
     int exclusive_range = 1000; 
     // look for the number 25
@@ -64,21 +68,26 @@ int main()
     const int BIN_NUM_SIZES = 16;
     int arr_n2[BIN_NUM_SIZES]=   {10, 100, 500, 1000, 5000, 10000, 50000, 100000, 200000, \
                             500000, 1000000, 2000000, 5000000, 10000000, 100000000, 500000000};
-
+        for (int i = 0; i < BIN_NUM_SIZES; i++) {
+            arr_n2[i] *= 4;
+        } 
     // create vector of runs for binarySearch
-
+    vector<double>bin_run;
 
     //create a for loop to go through all the element sizes
     for(int i = 0; i < BIN_NUM_SIZES; i++) {
         // create sa with vectors of up to arr_n[i] elements
-
+        int n = arr_n2[i];
+        SearchAnalysis sa2 = SearchAnalysis(n, exclusive_range);
 
         // record how long binarySearch runs for and append the time to your vector for binarySearch runs
-
+    double elapsed_bin_time = sa2.runTime(false, elem);
+    bin_run.push_back(elapsed_bin_time);
     }
     
     // save the times under BigO_binarySearch.csv
-
+    string filename_bin = "BigO_binarySearch.csv";
+    sa.writeToFile(filename_bin, bin_run, arr_n2);
 
 }
 
