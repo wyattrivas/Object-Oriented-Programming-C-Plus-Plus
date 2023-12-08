aebc1a64a1a7806798b78e828fb789c44191675f
wyattrivas <008181609@coyote.csusb.edu>
2023-12-08 12:28:49 -0800

    created folder

---
+++ Labs/Lab7/Lab_7.cpp
@@ -0,0 +1,27 @@
+//Lab_7.cpp
+#include <iostream>
+#include "ArrayVec.h"
+#include "DisplayArr.h"
+using namespace std;
+int main()
+{
+    // initialize arr
+    const int CAPACITY = 10;
+    int test_arr[CAPACITY];
+
+    //initialize ArrayVec and DisplayArr
+    DisplayArr disp = DisplayArr();
+    ArrayVec av = ArrayVec();
+
+    // populate with values
+    int range = 15;
+    int num_elem = 10;
+    av.randGenArr(test_arr, num_elem, range);
+
+    // display contents
+    int test_arr_size = av.getSize(test_arr, CAPACITY);
+    cout << "Array Elements\n";
+    disp.printArray(test_arr, test_arr_size);
+    cout << "Array Memsize\n";
+    disp.printMetaArray(test_arr, test_arr_size);
+}
